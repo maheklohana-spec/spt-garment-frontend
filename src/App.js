0b8619f9bfc8f4271f540c933fb86e773525722a
Mahek Lohana <2025.mahek.lohana@ves.ac.in>
2026-05-23 09:38:55 +0530

    add edit button to bills

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/masters" element={<Masters />} />
         <Route path="/sales" element={<Sales />} />
-<Route path="/sales/edit/:id" element={<Sales />} />
+        <Route path="/sales/edit/:id" element={<Sales />} />
+
         <Route path="/bills" element={<Bills />} />
         <Route path="/invoice/*" element={<Invoice />} />
         <Route path="/reports" element={<Reports />} />
